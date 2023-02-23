281e21c2a1b2a161809a043f1a466492f6ba5192
PeterCMDC <peter@cmdc.eu>
2023-02-23 10:27:47 +0100

    Individually log all messages (ignore duplicates)

--- src/Logger.php
+++ src/Logger.php
@@ -8,42 +8,42 @@ class CcvOnlinePaymentsPaymentPrestashopLogger implements \Psr\Log\LoggerInterfa
 
     public function emergency($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR, null, null, null, true);
     }
 
     public function alert($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR, null, null, null, true);
     }
 
     public function critical($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR, null, null, null, true);
     }
 
     public function error($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_ERROR, null, null, null, true);
     }
 
     public function warning($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_WARNING);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_WARNING, null, null, null, true);
     }
 
     public function notice($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_INFO);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_INFO, null, null, null, true);
     }
 
     public function info($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_INFO);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_INFO, null, null, null, true);
     }
 
     public function debug($message, array $context = array())
     {
-        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_DEBUG);
+        Logger::addLog($this->contextToMessage($message, $context), self::SEVERITY_DEBUG, null, null, null, true);
     }
 
     public function log($level, $message, array $context = array())
