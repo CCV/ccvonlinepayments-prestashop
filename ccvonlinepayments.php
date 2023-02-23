63ef3aa90fd53e199f6513da76e04c5c8e8bef2a
PeterCMDC <peter@cmdc.eu>
2023-02-23 10:28:19 +0100

    Support for version 8.0

--- ccvonlinepayments.php
+++ ccvonlinepayments.php
@@ -29,7 +29,7 @@ class CcvOnlinePayments extends PaymentModule
         $this->name = 'ccvonlinepayments';
         $this->tab = 'payments_gateways';
         $this->version = '1.2.6';
-        $this->ps_versions_compliancy = array('min' => '1.7.6.0', 'max' => '1.7.8.999');
+        $this->ps_versions_compliancy = array('min' => '1.7.6.0', 'max' => '8.0.999');
         $this->author = 'CCV';
         $this->controllers = array('payment', 'webhook', 'return', 'statuspoll');
         $this->is_eu_compatible = 1;
