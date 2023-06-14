0b4e9a0676bfffbdcfb7d09a025584abe6dcff94
PeterCMDC <peter@cmdc.eu>
2023-06-14 12:20:45 +0200

    Version bump to 1.3.1

--- ccvonlinepayments.php
+++ ccvonlinepayments.php
@@ -28,8 +28,8 @@ class CcvOnlinePayments extends PaymentModule
     {
         $this->name = 'ccvonlinepayments';
         $this->tab = 'payments_gateways';
-        $this->version = '1.3.0';
-        $this->ps_versions_compliancy = array('min' => '1.7.6.0', 'max' => '8.0.999');
+        $this->version = '1.3.1';
+        $this->ps_versions_compliancy = array('min' => '1.7.6.0', 'max' => '8.1.999');
         $this->author = 'CCV';
         $this->controllers = array('payment', 'webhook', 'return', 'statuspoll');
         $this->is_eu_compatible = 1;
